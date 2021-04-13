1f8f60346ba7acc5e8372f1d15e6f5ce1475dae0
Umut Alptekin <amacalptekin@gmail.com>
2021-04-13 17:41:26 +0300

    added a new route only for api test, so that I can tell apart the calls from button and calls from tickets

--- server.js
+++ server.js
@@ -7,25 +7,29 @@ const PORT = process.env.PORT || 3001; // backend server runs from this port
 // Serve up static assets (usually on heroku)
 app.use(express.static("public"));
 
-
 // Parses the body for POST, PUT, DELETE, etc.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function(req, res, next){
-    console.log(__dirname)
-    res.sendFile(path.join(__dirname+'/index.html'));
+app.get('/', function (req, res, next) {
+  console.log(__dirname)
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.post('/mycallback', function(req, res, next){
-    res.send('Got a POST request')
-    console.log("got a post request")
-    console.log(req.body); // req.body contains the parsed body of the request.
+app.post('/mycallback', function (req, res, next) {
+  res.send('Got a POST request')
+  console.log("got a post request")
+  console.log(req.body); // req.body contains the parsed body of the request.
 });
 
+app.post('/apitest', function (req, res, next) {
+  res.send('Got a POST request from the api call')
+  console.log("Got a POST request from the api call")
+  console.log(req.body); // req.body contains the parsed body of the request.
+});
 
 
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
